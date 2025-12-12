8e10f38848451fbcd5de07088cd311e78abb0456
bedi-xd <bede.arpad.arany@gmail.com>
2025-12-12 13:19:43 +0100

    működő regisztráció pwd bcrypt-el

--- Casita_Gabriela/frontend/src/pages/Home.jsx
+++ Casita_Gabriela/frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-        <p class="text-blue-600/100 dark:text-sky-400/100">The quick brown fox...</p>
+        <p class="text-blue-600 dark:text-sky-400">The quick brown fox...</p>
 <p class="text-blue-600/75 dark:text-sky-400/75">The quick brown fox...</p>
 <p class="text-blue-600/50 dark:text-sky-400/50">The quick brown fox...</p>
 <p class="text-blue-600/25 dark:text-sky-400/25">The quick brown fox...</p>

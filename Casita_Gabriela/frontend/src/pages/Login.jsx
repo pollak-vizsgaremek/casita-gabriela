95ab67de1825b8d766086c107d9b32b0ec30a7b6
repettsarku <csurgaichristopher@gmail.com>
2025-12-12 12:43:08 +0100

    registration base

--- Casita_Gabriela/frontend/src/pages/Login.jsx
+++ Casita_Gabriela/frontend/src/pages/Login.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router';
 
 const Login = () => {
   return (
-    <div>
-      
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
+        <h2 className="text-2xl font-bold mb-6 text-center"></h2>
+        <p className="text-center text-black mb-6">
+           <Link to="/registration" className="text-blue-500 hover:underline">shortcut</Link>
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
+

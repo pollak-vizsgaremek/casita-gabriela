ee66b2f324e782690e3555f8de38bb42c65a3b17
bedi-xd <bede.arpad.arany@gmail.com>
2025-12-12 12:08:10 +0100

    regisztráció
    modellek létrehozása, adatbázis módosítás és regisztráció elkészítése

--- Casita_Gabriela/backend/server.js
+++ Casita_Gabriela/backend/server.js
@@ -1,13 +1,39 @@
 import express from 'express';
-import { PrismaClient } from './generated/prisma/client.js'; 
+import cors from "cors";
+import { PrismaClient } from './generated/prisma/client.js';
+import bcrypt from "bcrypt"; 
 const app = express()
 const port = 6969
 const prisma = new PrismaClient();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(cors());
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+
+app.post("/register", async (req, res) => {
+  const { name, email, password, phone_number, birth_date, address, identity_card } = req.body;
+
+  try {
+    // bcrypt hash
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.users.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+        phone_number,
+        birth_date: new Date(birth_date), // frontendről pl. "1990-01-01"
+        address,
+        identity_card,
+      },
+    });
+
+    res.json({ message: "User registered successfully!", user });
+  } catch (err) {
+    console.error(err);
+    res.status(400).json({ error: "Registration failed. Email may already exist or invalid data." });
+  }
+});
+
+app.listen(port, () => {console.log(`Backend is running on this port: ${port}`)
 })

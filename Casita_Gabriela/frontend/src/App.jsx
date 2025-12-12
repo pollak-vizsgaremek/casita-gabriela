47208357e712be262979476e71e15735e9d8f6e3
bedi-xd <bede.arpad.arany@gmail.com>
2025-12-12 10:09:03 +0100

    oldalak létrehozása 2025.12.12

--- Casita_Gabriela/frontend/src/App.jsx
+++ Casita_Gabriela/frontend/src/App.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import Registration from './pages/registration';
+import Login from './pages/login';
+import Contact from './pages/contact';
+import Admin from './pages/Admin';
+import Room from './pages/Room';
 const App = () => {
     return (
         <Router>

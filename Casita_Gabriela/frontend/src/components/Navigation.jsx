da9590a168922aad6ec70c916d0529fdea3664b7
repettsarku <csurgaichristopher@gmail.com>
2025-12-12 10:53:48 +0100

    Navbar, and other default sites to build
    Irány dolgozni

--- Casita_Gabriela/frontend/src/components/Navigation.jsx
+++ Casita_Gabriela/frontend/src/components/Navigation.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
+import { Link } from 'react-router';
+
+
 
 const Navigation = () => {
+     const routes = [
+  {
+    name: 'Home',
+    route: '/'
+  },
+  {
+    name: 'About',
+    route: '/about'
+  },
+  {
+    name: 'Contact',
+    route: '/contact'
+  },
+  {
+    name: 'Room',
+    route: '/room'
+  },
+  {
+    name: 'Admin',
+    route: '/admin'
+  },
+  {
+    name: 'Login',
+    route: '/login'
+  },
+  {
+    name: 'Registration',
+    route: '/registration'
+  }
+ ];
+
   return (
     <div>
-      
+        <nav className='bg-green-400 w-dvw h-1/10'>
+            {routes.map((r) => (
+                <Link to={r.route} key={r.route} style={{ color: 'black', textDecoration: 'none' }}>
+                    {r.name}
+                </Link>
+
+            ))}
+        </nav>
     </div>
   )
 }

6a16b7a4d2dc025bb644608fe493aa317d75dd8e
repettsarku <csurgaichristopher@gmail.com>
2025-12-12 13:00:56 +0100

    Base login

--- Casita_Gabriela/frontend/src/pages/Login.jsx
+++ Casita_Gabriela/frontend/src/pages/Login.jsx
@@ -3,13 +3,30 @@ import { Link } from 'react-router';
 
 const Login = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
-        <h2 className="text-2xl font-bold mb-6 text-center"></h2>
-        <p className="text-center text-black mb-6">
-           <Link to="/registration" className="text-blue-500 hover:underline">shortcut</Link>
-        </p>
-      </div>
+    <div className='flex items-center justify-center m-0 bg-gray-100 h-[90dvh] w-dvw'>
+     <div className='flex flex-col'>
+               <div className='text-black bg-white shadow-md rounded-xl p-4 ml-8 w-[25dvw] h-fill flex flex-col items-center'>
+                 <p className='text-black mb-4'>Bejelentkezés</p>
+                 <form className='w-full flex flex-col items-center'>
+                   <input
+                     type='email'
+                     placeholder='Email'
+                     className='mb-2 p-2 border border-gray-300 rounded w-full'
+                   />
+                   <input
+                     type='password'
+                     placeholder='Jelszó'
+                     className='mb-2 p-2 border border-gray-300 rounded w-full'
+                   />
+                   <button type='submit' className='bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700 mt-4'>
+                     Belépés
+                   </button>
+                 </form>
+                 <p className='mt-4'>
+                   Még nincs fiókod? <Link to="/registration" className="text-blue-500 hover:underline">Regisztrálj!</Link>
+                 </p>
+               </div>
+           </div>
     </div>
   );
 };

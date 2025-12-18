1eff047c150a4549e7b739c0c0c87a9e1094f42b
repettsarku <csurgaichristopher@gmail.com>
2025-12-18 10:18:14 +0100

    Navigation bar responsive lett

--- Casita_Gabriela/frontend/src/components/Navigation.jsx
+++ Casita_Gabriela/frontend/src/components/Navigation.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Link } from 'react-router';
 
 
 
+
 const Navigation = () => {
+
+  const navRef = useRef();
+
+const showSideBar = () => {
+  navRef.current.classList.add('showSidebar');
+};
+
+const hideSideBar = () => {
+  navRef.current.classList.remove('showSidebar');
+
+};
      const routes = [
   {
     name: 'Home',
@@ -26,11 +38,30 @@ const Navigation = () => {
 
   return (
     <div className='z-10 overflow-visible'>
-        <nav className=' bg-[#D9F5E3]  w-dvw h-[10dvh] flex items-center px-4 shadow-md  z-50 border-b-2 border-gray-200'>
+        <nav className=' bg-[#D9F5E3]  w-dvw h-[10dvh] flex items-center px-4 shadow-md  z-40 border-b-2 border-gray-200'>
+          
             <Link to={routes[0].route} className='h-full flex items-center'>
                 <img src="/C.png" alt="Home" className="h-7/10 w-auto" />
             </Link>
             <div className='ml-auto flex gap-6 align-center '>
+                {routes.slice(1).map((r, index) => (
+                    <Link to={r.route} 
+                    className={index === 2 ? 'bg-[#6FD98C] hidden sm:inline text-white px-3 py-1 rounded-xl hover:-translate-y-px hover:bg-[#5FCB80] transition-all duration-200 mr-5 w-25 text-center font-bold h-full'
+                       : 
+                    'text-[#1F1F1F] px-3 py-1 hidden sm:inline font-bold h-full hover:text-[#515151] hover:-translate-y-px transition-all duration-200'} key={r.route}>
+                        {r.name}
+                    </Link>
+                ))}
+                <div className='icon' onClick={showSideBar}></div>
+            </div>
+            
+        </nav>
+
+
+
+        <nav  ref={navRef} className=' bg-[#D9F5E3] sidebar w-[200px] h-dvh fixed right-0 top-0 items-center px-4 shadow-md  z-50 border-b-2 border-gray-200'>
+            <div className='ml-auto flex flex-col gap-6 align-center w-full '>
+              <div className='icon2 mt-10' onClick={hideSideBar}></div>
                 {routes.slice(1).map((r, index) => (
                     <Link to={r.route} 
                     className={index === 2 ? 'bg-[#6FD98C] text-white px-3 py-1 rounded-xl hover:-translate-y-px hover:bg-[#5FCB80] transition-all duration-200 mr-5 w-25 text-center font-bold h-full'
@@ -39,8 +70,11 @@ const Navigation = () => {
                         {r.name}
                     </Link>
                 ))}
+                
             </div>
         </nav>
+
+
     </div>
   )
 }

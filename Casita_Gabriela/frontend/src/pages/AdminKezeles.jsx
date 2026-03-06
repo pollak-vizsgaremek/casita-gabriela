c99b796974a15f83cd5397c36305c46673a81403
bedi-xd <bede.arpad.arany@gmail.com>
2026-03-06 12:24:17 +0100

    hibajavítások, foglalási folyamat logika elkezdése

--- Casita_Gabriela/frontend/src/pages/AdminKezeles.jsx
+++ Casita_Gabriela/frontend/src/pages/AdminKezeles.jsx
@@ -224,7 +224,7 @@ const AdminKezeles = () => {
                 value={formData.title}
                 onChange={handleInputChange}
                 className='w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-900 bg-white'
-                placeholder='Pl. Szép Megtekintés Szoba'
+                placeholder='Pl. Szép Megtekintésű Szoba'
               />
             </div>
             <div>
@@ -268,7 +268,7 @@ const AdminKezeles = () => {
               </div>
               <div>
                 <label className='block text-xs font-semibold text-gray-700 mb-2'>
-                  Ár (€/éj)
+                  Ár (Ft/fő/éj)
                 </label>
                 <input
                   type='number'
